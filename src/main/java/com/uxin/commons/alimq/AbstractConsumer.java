81964c6a61309a641f378b4c2d139406600290bb
ellis.luo <282390074@qq.com>
2016-11-06 13:15:07 +0800

    添加消费者重试和异常处理

--- src/main/java/com/uxin/commons/alimq/AbstractConsumer.java
+++ src/main/java/com/uxin/commons/alimq/AbstractConsumer.java
@@ -26,45 +26,55 @@ public abstract class AbstractConsumer<T>
 
     public abstract void process(MQEntry<T> entry);
 
-    public void init() throws Exception
+    public void init()
     {
-        Properties properties = new Properties();
-        properties.put(PropertyKeyConst.ConsumerId, consumerId);// 您在MQ控制台创建的Consumer ID
-        properties.put(PropertyKeyConst.AccessKey, accessKey);// 鉴权用AccessKey，在阿里云服务器管理控制台创建
-        properties.put(PropertyKeyConst.SecretKey, secretKey);// 鉴权用SecretKey，在阿里云服务器管理控制台创建
-        consumer = ONSFactory.createConsumer(properties);
+        try
+        {
 
-        analysisAnnotation(); // 解析注解配置信息
-        logger.info(getClass().getSimpleName() + " init end, properties:" + properties);
+            Properties properties = new Properties();
+            properties.put(PropertyKeyConst.ConsumerId, consumerId);// 您在MQ控制台创建的Consumer ID
+            properties.put(PropertyKeyConst.AccessKey, accessKey);// 鉴权用AccessKey，在阿里云服务器管理控制台创建
+            properties.put(PropertyKeyConst.SecretKey, secretKey);// 鉴权用SecretKey，在阿里云服务器管理控制台创建
+            consumer = ONSFactory.createConsumer(properties);
+
+            analysisAnnotation(); // 解析注解配置信息
+            logger.info(getClass().getSimpleName() + " init end, properties:" + properties);
+        }
+        catch (Exception e)
+        {
+            logger.error("MQ Consumer " + getClass().getSimpleName() + " init error ", e);
+            throw new RuntimeException("MQ Consumer " + getClass().getSimpleName() + " init error ", e);
+        }
     }
 
     public void todo()
     {
-        try
+        if (consumer == null)
         {
-            if (consumer == null)
-            {
-                init();
-            }
+            init();
+        }
 
-            consumer.subscribe(topic, "*", new MessageListener()
+        consumer.subscribe(topic, "*", new MessageListener()
+        {
+            public Action consume(Message message, ConsumeContext context)
             {
-                public Action consume(Message message, ConsumeContext context)
+                logger.info("MQ " + getClass().getSimpleName() + " Receive: " + message);
+                MQEntry entry = new MQEntry(message.getTopic(), message.getTag(),
+                        Hessian2Serialization.deserialize(message.getBody(), Object.class));
+                try
                 {
-                    logger.info("MQ " + getClass().getSimpleName() + " Receive: " + message);
-                    MQEntry entry = new MQEntry(message.getTopic(), message.getTag(),
-                            Hessian2Serialization.deserialize(message.getBody(), Object.class));
                     process(entry);
-                    return Action.CommitMessage;
                 }
-            });
-            consumer.start();
-        }
-        catch (Throwable t)
-        {
-            logger.error("MQ Consumer error ", t);
-        }
+                catch (Throwable t)
+                {
+                    logger.error("MQ Consumer process error ", t);
+                    return Action.ReconsumeLater;
+                }
 
+                return Action.CommitMessage;
+            }
+        });
+        consumer.start();
     }
 
     private void analysisAnnotation() throws Exception
