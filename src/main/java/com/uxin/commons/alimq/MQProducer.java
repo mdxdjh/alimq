c7681b0c1ba6d82bad224713ea70bbdc4fe07788
ellis.luo <282390074@qq.com>
2016-11-06 12:12:57 +0800

    中文乱码

--- src/main/java/com/uxin/commons/alimq/MQProducer.java
+++ src/main/java/com/uxin/commons/alimq/MQProducer.java
@@ -25,11 +25,11 @@ public class MQProducer
     static
     {
         Properties properties = new Properties();
-        properties.put(PropertyKeyConst.ProducerId, producerId);// ����MQ����̨������Producer ID
-        properties.put(PropertyKeyConst.AccessKey, accessKey);// ��Ȩ��AccessKey���ڰ����Ʒ������������̨����
-        properties.put(PropertyKeyConst.SecretKey, secretKey);// ��Ȩ��SecretKey���ڰ����Ʒ������������̨����
+        properties.put(PropertyKeyConst.ProducerId, producerId);// 您在MQ控制台创建的Producer ID
+        properties.put(PropertyKeyConst.AccessKey, accessKey);// 鉴权用AccessKey，在阿里云服务器管理控制台创建
+        properties.put(PropertyKeyConst.SecretKey, secretKey);// 鉴权用SecretKey，在阿里云服务器管理控制台创建
         producer = ONSFactory.createProducer(properties);
-        // �ڷ�����Ϣǰ���������start����������Producer��ֻ�����һ�μ���
+        // 在发送消息前，必须调用start方法来启动Producer，只需调用一次即可
         producer.start();
     }
 
